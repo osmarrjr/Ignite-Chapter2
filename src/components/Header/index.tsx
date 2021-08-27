e959af387603d88ee60255a81cf16b40bc1df2af
osmarrjr <osmar.rjr@hotmail.com>
2021-08-27 09:33:41 -0300

    Iniciando o projeto e configurando o Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import logo from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+export function Header() {
+    return (
+        <Container>
+            <Content>
+                <img src={logo} alt="dtmoney" />
+                <button type="button">Nova Transação</button>
+            </Content>
+        </Container>
+    )
+}
